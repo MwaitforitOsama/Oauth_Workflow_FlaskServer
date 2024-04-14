81b1d3f96833f25eb90319062e5621ab204a21f4
MwaitforitOsama <osama.osamalink@gmail.com>
2024-04-15 03:59:59 +0500

    Updated Oauth Flow-- Working version v1.0

--- app.py
+++ app.py
@@ -5,18 +5,37 @@ app = Flask(__name__)
 
 CLIENT_ID = "O3rIybaWYTNkqrYGbZMMsCgypBaaWjQ14z3odCLbSEc"
 CLIENT_SECRET = "your_client_secret"
-REDIRECT_URI = "https://43e8-39-42-138-20.ngrok-free.app/receive"  #Your NGROK URL HERE
+REDIRECT_URI = "https://89cc-39-42-138-20.ngrok-free.app"  # Remove leading/trailing spaces
 
 
 def get_auth_url():
     auth_url = "https://api.gomotive.com/oauth/authorize"
-    scope = "companies.read users.read users.manage vehicles.read vehicles.manage"
+    scope = "companies.read"
     return f"{auth_url}?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code&scope={scope}"
+
+@app.route('/receive2')
+def exchange_code_for_token():
+    url = 'https://api.gomotive.com/oauth/token'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'grant_type': 'authorization_code',
+        'code': "m8U24S8t9dmboLazJgmcOb4sNVa6GU-wAqHi5NLm5g8",
+        'redirect_uri': "https://89cc-39-42-138-20.ngrok-free.app",
+        'client_id': "O3rIybaWYTNkqrYGbZMMsCgypBaaWjQ14z3odCLbSEc",
+        'client_secret': "bQGkPHbKT1Eg4FY8fqQhz1_gGMHra1o-CFCDHmZ9r_Q"
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+    json_data = response.json()
+    # You can do further processing with the JSON data if needed
+    return jsonify(json_data)
+
 
 @app.route('/hit_url')
 def hit_url_endpoint():
     url_to_hit = get_auth_url()
-
     return url_to_hit
 
 
@@ -25,33 +44,18 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/receive')
+@app.route('/receive', methods=['GET', 'POST'])
 def receive_json():
-    if request.method == 'POST' or 'GET':
-        print(request.json())
+    if request.method == 'GET':
+        return render_template('receive.html')
+    elif request.method == 'POST':
         try:
             # Get JSON data from the request
             json_data = request.get_json()
+            print(json_data)
             return jsonify({"received_data": json_data}), 200
         except Exception as e:
             return jsonify({"error": str(e)}), 400
-
-# @app.route('/hit_url')
-# def hit_url_endpoint():
-#     url_to_hit = get_auth_url()
-#     print(url_to_hit)
-#     return redirect(url_to_hit)
-
-# @app.route('/callback')
-# def oauth_callback():
-#     code = request.args.get('code')  # Extract the authorization code from the URL parameters
-
-#     access_token = exchange_code_for_token(code)
-
-#     if access_token:
-#         return f"Authentication successful! Access Token: {access_token}"
-#     else:
-#         return "Authentication failed. Unable to obtain access token."
 
 if __name__ == '__main__':
     app.run(debug=True)
